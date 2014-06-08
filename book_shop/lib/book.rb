c8da9f84b0e06581adf1b3253bb9cbea23c23df3
Efraim Gentil <efraim.gentil@gmail.com>
2014-06-08 14:51:04 -0300

    Set and more if examples

--- book_shop/lib/book.rb
+++ book_shop/lib/book.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class Book
 	
-    attr_accessor :price , :category
+    attr_accessor :price , :category , :isnb
 
     def initialize ( author , isnb = "1" , number_of_pages, price , category )
         @author = author
@@ -13,6 +13,14 @@ class Book
 
     def to_s
         "Book: { author: #{@author}, isnb: #{@isnb}, number_of_pages: #{@number_of_pages}, price: #{@price} }"
+    end
+
+    def eql? other_book
+        @isnb == other_book.isnb
+    end
+
+    def hash
+        @isnb.hash
     end
 
 end
---
+++ data-structures/set.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+puts "A Set is similar to a array it is a list of element"
+puts "but a Set can't contain repeated elements in it"
+puts "to create a Set you need to, import the set lib through"
+puts "require 'set'"
+puts "and then you instatiate a set"
+puts "@my_set = Set.new"
+puts "---------------------------------------------------"
+puts "To add elements to the set is the same way as the array"
+puts "@my_set << 1"
+puts "But if you try to add the same element to the set"
+puts "it will maintain only one element without repetition"
+
+
